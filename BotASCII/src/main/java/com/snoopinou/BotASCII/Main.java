b60b3b3756cec0c8baa191f1684720690fe3c799
snoopinou30 <jules.snoopi@live.fr>
2018-07-09 18:18:34 +0200

    Changed bot token

--- BotASCII/src/main/java/com/snoopinou/BotASCII/Main.java
+++ BotASCII/src/main/java/com/snoopinou/BotASCII/Main.java
@@ -6,10 +6,13 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
+
+
 public class Main {
 	
 	public static void main(String args[]) throws LoginException {
-		JDA jda = new JDABuilder(AccountType.BOT).setToken("NDYxOTI3OTQxNDg0NDQ1NzA2.Dhabgg.rx18bX8tpFABbQe8kCyYfytlGMI").buildAsync();
+		JDA jda = new JDABuilder(AccountType.BOT).setToken("NDY1OTE0MjgxODE3NDA3NDg4.DiUcBQ.jHC1-LjIv2N4M6hzT5CuPk1dV8c").buildAsync();
+		System.out.println(jda.asBot().getInviteUrl());
 		jda.addEventListener(new MyListener());
 	}
 
